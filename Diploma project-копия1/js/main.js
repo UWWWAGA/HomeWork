7da243bee2466478accad37a612a874814df2e0f
UWWWAGA <147951254+UWWWAGA@users.noreply.github.com>
2024-07-17 19:09:33 +0300

    Add files via upload

---
+++ Diploma project-копия1/js/main.js
@@ -0,0 +1,16 @@
+new Swiper('.mySwiper', {
+
+  spaceBetween: 0,
+  slidesPerView: 3,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+
+
+  }
+  
+});
