0ea3316ef0deacd7538448959de107df16e2198b
UWWWAGA <147951254+UWWWAGA@users.noreply.github.com>
2024-09-02 21:13:27 +0300

    Add files via upload

---
+++ Diploma project-копия1/js/main.js
@@ -0,0 +1,79 @@
+var swiper1 = new Swiper(".mySwiper1", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next-1",
+    prevEl: ".swiper-button-prev-1",
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+    },
+    844: {
+      slidesPerView: 2,
+    },
+    100: {
+      slidesPerView: 1,
+    }
+  }
+
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next-2",
+    prevEl: ".swiper-button-prev-2",
+  },
+});
+var swiper3 = new Swiper(".mySwiper3", {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next-3",
+    prevEl: ".swiper-button-prev-3",
+  },
+});
+
+
+(function () {
+  // Бургер
+
+  document.addEventListener('click', burgerInit)
+
+  function burgerInit(e) {
+
+    const burgerIcon = e.target.closest('.burger-icon')
+    const burgerNavLink = e.target.closest('.nav__link')
+
+    if (!burgerIcon && !burgerNavLink) return
+    if (document.documentElement.clientWidth > 1100) return
+
+    if (burgerIcon) {
+      e.preventDefault();
+    }
+
+    if (!document.body.classList.contains('body--opened-menu')) {
+      document.body.classList.add('body--opened-menu')
+    } else {
+      document.body.classList.remove('body--opened-menu')
+    }
+  }
+
+
+})()
+
